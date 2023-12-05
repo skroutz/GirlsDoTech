f9f8fcf6d2f96249bdd119eef17007b06f972215
Fotis Tsamis <ftsamis@skroutz.gr>
2023-12-05 17:50:03 +0000

    Add chapter 7 files

--- projects/railsgirls/config/routes.rb
+++ projects/railsgirls/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/about'
-  root to: redirect("/ideas")
+  root "pages#homepage"
   resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

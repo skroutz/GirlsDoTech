3181b67d842df05600ea97d9fc1fd73fec4b1ed6
Fotis Tsamis <ftsamis@skroutz.gr>
2023-12-05 18:06:11 +0000

    Add chapter 13 files

--- projects/railsgirls/app/uploaders/picture_uploader.rb
+++ projects/railsgirls/app/uploaders/picture_uploader.rb
@@ -1,7 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  version :thumb do
+    process :resize_to_fit => [150, 150]
+  end
+  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
